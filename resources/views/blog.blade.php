8268577fe6dba73bcd932bea66cf340e8e6c3c95
Novaasyifa <126181389+Novaasyifa@users.noreply.github.com>
2024-11-03 22:44:50 +0700

    Add files via upload

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Blog</title>
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/blog">Blog</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+    <h1>Halaman Blog</h1>
+    <article>
+        <h3>Judul 1</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia et ab assumenda quo, quisquam necessitatibus labore qui modi quod pariatur, est a suscipit iure molestias. Earum recusandae at reprehenderit commodi!</p>
+    </article>
+
+    <article>
+        <h3>Judul 2</h3>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quos sed maxime libero praesentium repudiandae voluptatum, rerum esse minima aut blanditiis molestias fuga, quisquam laudantium, saepe dolor perspiciatis veritatis. Ut!
+    </article>
+</body>
+</html>
